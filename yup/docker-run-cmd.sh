7172731fdecbb5c4029c78b1c90984cda13f41ec
Bram Geron <bgeron@bgeron.nl>
2015-11-11 19:58:11 +0000

    Improve UI

--- yup/docker-run-cmd.sh
+++ yup/docker-run-cmd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo 'Welcome to this virtual machine. To switch the user and drop privileges, type `user`.'
-echo 'From there, type "ocaml" to start ocaml-multicore.'
+echo 'From there, type "yup" to start yup. The installation directory is in ~user/installs .'
 echo
 
